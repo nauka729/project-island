ffeb90bda75a7c4e21050282e30cd620c25cd2e9
nauka729 <nauka792@gmail.com>
2023-08-08 20:05:12 +0200

    creating a file database, send and get endpoint

--- app.py
+++ app.py
@@ -1,5 +1,45 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+import json
+from flask_cors import CORS
+
 app = Flask(__name__)
+CORS(app)
+
 @app.route("/")
 def hello():
     return "Hello, World!"
+
+@app.route('/get_items', methods=["GET"])
+def get_items():
+    print("Get items endpoint reached...")
+    with open("database_placeholder.json", "r") as f:
+        data = json.load(f)
+    return jsonify(data)
+        
+
+
+@app.route('/send_items', methods=["POST"])
+def send_items():
+    print("Items reached endpoint...")
+    data = request.json
+    
+    item = data['item']
+    price = data['price']
+
+    print(item)
+    print(price)
+
+    with open("database_placeholder.json", "r") as f:
+        items = json.load(f)
+    
+    items.append({
+        'item': item,
+        'price': price
+    })
+
+    with open("database_placeholder.json", "w") as f:
+        json.dump(items, f)
+
+
+
+    return jsonify({"message": "Items added successfully!"})
---
+++ script.js
@@ -0,0 +1,40 @@
+function json_to_html_list(data_in_json) {
+    let output = "<ul>";
+    for (let item of data_in_json) {
+        output += `<li>${item.item} -- $${item.price}</li>`;
+    }
+    output += "</ul>";
+    return output
+}
+
+function loadData() {
+    fetch("http://127.0.0.1:5000/get_items")
+    // the frontend is running on port: 3000 while backend on 5000, you need to specify that 
+    .then(res => res.json())
+    .then(res => {
+        document.getElementById("data_display").innerHTML = json_to_html_list(res);
+        //console.log(res);    
+    })
+}
+
+function sendData() {
+    const itemName = document.getElementById("item_name").value;
+    const itemPrice = document.getElementById("item_price").value;
+    console.log(itemName); 
+    console.log(itemPrice); 
+
+    const data = {
+        item: itemName,
+        price: itemPrice
+    }
+
+    fetch('http://127.0.0.1:5000/send_items', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+
+    })
+
+}
