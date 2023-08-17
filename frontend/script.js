4f2360ff01d97fde138a94f78a897a61022530a2
nauka729 <nauka792@gmail.com>
2023-08-17 22:45:49 +0200

    changes in frontend display function

--- frontend/script.js
+++ frontend/script.js
@@ -1,27 +1,35 @@
 function json_to_html_list(data_in_json) {
-    let output = "<tr>";
+    let output = '';
     for (let item of data_in_json) {
-        output += `<td>${item[0]} -- ${item[1]} -- ${item[2]} -- ${item[3]} -- ${item[4]} -- ${item[5]} -- ${item[6]}</td>`;
+        output += `
+        <tr>
+        <td>${item[0]}</td>
+        <td>${item[1]}</td>
+        <td>${item[2]} </td>
+        <td>${item[3]}</td>
+        <td>${item[4]}</td>
+        <td>${item[5]}</td>
+        <td>${item[6]}</td>
+        </tr>`;
     }
-    output += "</tr>";
     return output
 }
 
 function loadData() {
     fetch("http://127.0.0.1:5000/get_items")
-    // the frontend is running on port: 3000 while backend on 5000, you need to specify that 
-    .then(res => res.json())
-    .then(res => {
-        document.getElementById("data_display").innerHTML = json_to_html_list(res);
-        //console.log(res);    
-    })
+        // the frontend is running on port: 3000 while backend on 5000, you need to specify that 
+        .then(res => res.json())
+        .then(res => {
+            document.getElementById("items_display").innerHTML = json_to_html_list(res);
+            
+        })
 }
 
 function sendData() {
     const itemName = document.getElementById("item_name").value;
     const itemPrice = document.getElementById("item_price").value;
-    console.log(itemName); 
-    console.log(itemPrice); 
+    console.log(itemName);
+    console.log(itemPrice);
 
     const data = {
         item: itemName,
