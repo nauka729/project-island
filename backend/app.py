46dd3d64f87e2ecd66975f2bef267303aaa76e0f
nauka729 <nauka792@gmail.com>
2023-08-26 22:17:23 +0200

    adding connection pools

--- backend/app.py
+++ backend/app.py
@@ -6,6 +6,8 @@ from psycopg2.extras import RealDictCursor
 from parse_and_insert import parse_and_insert
 import datetime
 from decouple import config
+from psycopg2 import pool
+
 
 # ENV. VARIABLES:
 DATABSE=config('DATABSE')
@@ -13,73 +15,58 @@ USER=config('USER')
 PASSWORD=config('PASSWORD')
 HOST=config('HOST')
 PORT=config('PORT')
+
+# CREATING THE CONNECTION POOL
+minconn = 1 # min. amount of concurrent connections to the database
+maxconn = 10 # this will need to be increased it this is ever published
+
+db_pool = pool.SimpleConnectionPool(
+    minconn, maxconn,
+    database=DATABSE,
+    user=USER,
+    password=PASSWORD,
+    host=HOST,
+    port=PORT
+)
+
+if not db_pool:
+    print("Error: Failed to create database connection pool.")
+
  
 app = Flask(__name__)
 CORS(app)
+
 
 @app.route("/")
 def hello():
     return "Hello, World!"
 
+
 @app.route('/api/v1/get_items', methods=["GET"])
 def get_items():
-    print("Get items endpoint reached...")
-
-    #Connect to the database
-    conn = psycopg2.connect(database=DATABSE,
-                            user=USER,
-                            password=PASSWORD,
-                            #host="localhost", port="5432")
-                            #host="host.docker.internal", port="5555") # if you need to connect from container to host's localhost
-                            #host="postgres-test-service", port="5432")    # TEST SERVICE NEEDS TO BE CHANGED LATER!
-                            host=HOST, port=PORT)
-    # create a cursor
-    cur = conn.cursor()
-  
-    # Select all products from the table
-    cur.execute('''SELECT id, item_id, auction_id, hid, name, owner, price, time, created_at, updated_at FROM items''')
-  
-    # Fetch the data
-    data = cur.fetchall()
-  
-    # close the cursor and connection
-    cur.close()
-    conn.close()
-    #print(data)
-    #print(json.dumps(data))
-    # handling the datatime object and converting it to string
+    with db_pool.getconn() as conn:
+        with conn.cursor() as cur:
+            # Select all products from the table
+            cur.execute('''SELECT id, item_id, auction_id, hid, name, owner, price, time, created_at, updated_at FROM items''')
+            data = cur.fetchall()
+        db_pool.putconn(conn)
+        # handling the datatime object and converting it to string
     return json.dumps(data, default=lambda o: o.strftime('%Y-%m-%d %H:%M:%S') if isinstance(o, datetime.datetime) else o)
-        
+
+
+
 @app.route('/api/v1/get_item_by_name', methods=["GET"])
 def get_item_by_name():
-    print("Get single item by name endpoint reached...")
     item_name = request.args.get('name')
-    print(item_name)
-    #Connect to the database
-    conn = psycopg2.connect(database=DATABSE,
-                            user=USER,
-                            password=PASSWORD,
-                            #host="localhost", port="5432")
-                            #host="host.docker.internal", port="5555") # if you need to connect from container to host's localhost
-                            #host="postgres-test-service", port="5432")    # TEST SERVICE NEEDS TO BE CHANGED LATER!
-                            host=HOST, port=PORT)
-    # create a cursor
-    cur = conn.cursor()
-  
-    # Select all products from the table
-    # You need the comma at the end as this makes it a tuple!
-    cur.execute("SELECT name, price, created_at, updated_at FROM items WHERE name = %s", (item_name,))
-  
-    # Fetch the data
-    data = cur.fetchall()
-  
-    # close the cursor and connection
-    cur.close()
-    conn.close()
-    #print(data)
-    #print(json.dumps(data))
-    # handling the datatime object and converting it to string
+    with db_pool.getconn() as conn:
+        with conn.cursor() as cur:
+            # Select all products from the table
+            cur.execute("SELECT name, price, created_at, updated_at FROM items WHERE name = %s", (item_name,))
+            data = cur.fetchall()
+        db_pool.putconn(conn)
+        # handling the datatime object and converting it to string
     return json.dumps(data, default=lambda o: o.strftime('%Y-%m-%d %H:%M:%S') if isinstance(o, datetime.datetime) else o)
+
 
 @app.route('/api/v1/send_items_json', methods=["POST"])
 def send_items_json():
@@ -89,3 +76,8 @@ def send_items_json():
     message = parse_and_insert(json_text)
 
     return message
+
+@app.teardown_appcontext
+def close_db_pool(error):
+    if hasattr(g, 'db_pool'):
+        g.db_pool.closeall()
